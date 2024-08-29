3a19a8ace01c5712cee647db46c465c48fca7c67
Eduardo Antunes <eduardoantunes.2001@gmail.com>
2024-08-29 08:41:51 -0300

    execute

--- random_Search.py
+++ random_Search.py
@@ -28,7 +28,6 @@ def isFeasible(x):
     h[2] = x[2] + x[6] + x[7] + 2 * x[8] + x[9] - 1
     return h
 
-# Função de busca aleatória
 def busca_aleatoria(qtd_interacoes, limite_inf, limite_sup, tam):
     c = [-6.089, -17.164, -34.054, -5.914, -24.721, -14.986, -24.1, -10.708, -26.662, -22.179]
     x = [None] * tam
@@ -41,28 +40,28 @@ def busca_aleatoria(qtd_interacoes, limite_inf, limite_sup, tam):
         for i in range(tam):
             x[i] = random.uniform(limite_inf, limite_sup)
         y = funcao_objetivo(tam, x, c)
-        if tem_factivel:
-            if melhor_y > y and all(i <= 0.001 for i in isFeasible(x)):
-                if melhor_y > y:
-                    melhor_y = y
-                    melhor_x = x.copy()
+        
+        # Verificar se a solução é factível
+        if all(i <= 0.001 for i in isFeasible(x)):
+            if melhor_y > y:
+                melhor_y = y
+                melhor_x = x.copy()
+                tem_factivel = True
         else:
-            if melhor_y > y and all(i <= 0.001 for i in isFeasible(x)):
-                if melhor_y > y:
-                    melhor_y = y
-                    melhor_x = x.copy()
-                    tem_factivel = True
-            if not all(i <= 0.001 for i in isFeasible(x)):
-                y += np.sum(np.abs(isFeasible(x))) + 2200
-                if melhor_y > y:
-                    melhor_y = y
-                    melhor_x = x.copy()
+            # Penalização por barreira: define o fitness como infinito
+            y = np.inf
+            if melhor_y > y:
+                melhor_y = y
+                melhor_x = x.copy()
+        
         melhores_imagens.append(melhor_y)
+    
     return melhor_x, melhor_y, melhores_imagens
+
 
 # Configuração dos diretórios e execução
 qtd = 500
-output_dir = r'C:\Users\eduar\OneDrive\Área de Trabalho\graficos'
+output_dir = r'C:\Users\eduar\OneDrive\Documentos\GitHub\Genetic-Algorithm-vs-Random-Search-with-penalty\random_search_results'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
@@ -81,8 +80,9 @@ for k in range(3):
         x, y, melhor_y = busca_aleatoria(qtd, 0, 10, 10)
         fim = time.time()
         tempo.append(fim - inicio)
-        if all(i <= 0.001 for i in isFeasible(x)):
-            factivel += 1.0
+        if all(j != None for j in x):
+            if all(i <= 0.001 for i in isFeasible(x)):
+                factivel += 1.0
         dados.append(melhor_y)
         date.append(y)
         plt.title('Convergência')
@@ -90,21 +90,15 @@ for k in range(3):
         plt.ylabel('Melhor valor')
         plt.axvline(0, color='k')
         plt.axhline(0, color='k')
-        plt.plot(range(len(melhor_y)), melhor_y, marker=',')
+
+        # Substituir np.inf por um valor muito grande para visualização
+        valores_muito_grandes = [1e10 if np.isinf(val) else val for val in melhor_y]
+
+        plt.plot(range(len(valores_muito_grandes)), valores_muito_grandes, marker=',')
         plt.grid(True)
         plt.savefig(os.path.join(output_dir, f'convergence_{k+1}_{i}.png'))
         plt.close()
-    plt.title('Convergência')
-    plt.xlabel('Iterações')
-    plt.ylabel('Melhor valor')
-    plt.axvline(0, color='k')
-    plt.axhline(0, color='k')
-    for j in range(10):
-        plt.plot(range(len(dados[j])), dados[j], marker=',')
-    plt.grid(True)
-    plt.savefig(os.path.join(output_dir, f'convergence_{k+1}.png'))
-    plt.show()
-    plt.close()
+
 
     tabela = {
         'Variáveis': ['Melhor', 'Pior', 'Média', 'Mediana', 'Desvio Padrão', 'Factível por Execução', 'Tempo médio de execução'],
